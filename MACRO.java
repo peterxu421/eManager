8c0eda7dbeeecd3b7c4a833f16e73c1279f95dc6
Guan Yilun <zoom.aaron@gmail.com>
2012-03-09 11:02:49 +0800

    [Create] Venue Applicant Class

--- MACRO.java
+++ MACRO.java
@@ -3,4 +3,5 @@ public class MACRO {
 	public static int ORGANIZER = 1;
 	public static int FACILITATOR = 2;
 	public static int PARTICIPANT = 3;
+	public static int APPLICANT = 4;
 }
---
+++ VenueApplicant.java
@@ -0,0 +1,28 @@
+
+public class VenueApplicant extends Member {
+	public VenueApplicant(String name, String matricNo, String faculty, int year,
+			int contact, String email, String foodType, String allergy, String position) {
+		this.name = name;
+		this.matricNo = matricNo;
+		this.faculty = faculty;
+		this.year = year;
+		this.contact = contact;
+		this.email = email;
+		this.foodType = foodType;
+		this.allergy = allergy;
+		this.role = MACRO.APPLICANT;
+	}
+	public VenueApplicant(int ID, String name, String matricNo, String faculty, int year,
+			int contact, String email, String foodType, String allergy, String position) {
+		this.ID = ID;
+		this.name = name;
+		this.matricNo = matricNo;
+		this.faculty = faculty;
+		this.year = year;
+		this.contact = contact;
+		this.email = email;
+		this.foodType = foodType;
+		this.allergy = allergy;
+		this.role = MACRO.APPLICANT;
+	}
+}
