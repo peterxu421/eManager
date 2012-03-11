0c5bd94bcd99dc2222ba2530986a1d2964d05722
Ruifeng Ma <MRFflyer@gmail.com>
2012-03-11 12:13:50 +0800

    [update] venue management

--- MACRO.java
+++ MACRO.java
@@ -4,7 +4,7 @@ public class MACRO {
 	public static int FACILITATOR = 2;
 	public static int PARTICIPANT = 3;
 	public static int APPLICANT = 4;
-	public static int PENDING = 5; 
-	public static int APPROVED = 6;
-	public static int REJECTED = 7;
+	public static int PENDING = 1; 
+	public static int APPROVED = 2;
+	public static int REJECTED = 3;
 }
--- VenueManagement_BookingApplications.java
+++ VenueManagement_BookingApplications.java
@@ -17,6 +17,8 @@ public class VenueManagement_BookingApplications extends Composite {
 
 	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
 	private Table applicationTable;
+	
+	private ArrayList<VenueBookingInfo> bookingInfoList;
 
 	/**
 	 * Create the composite.
@@ -97,7 +99,7 @@ public class VenueManagement_BookingApplications extends Composite {
 	
 	public void importApplicationData() {
 		DatabaseReader db = new DatabaseReader();
-		ArrayList<Venue> venueList = db.getVenues();
+/*		ArrayList<Venue> venueList = db.getVenues();
 		
 		for(int i=0; i<venueList.size(); i++){
 		    Venue venue = venueList.get(i);
@@ -115,6 +117,27 @@ public class VenueManagement_BookingApplications extends Composite {
 					item.setText(7, "Pending");
 				}
 			}	
+		}*/
+		
+		bookingInfoList = db.getVenueBookingInfo();
+		if(!bookingInfoList.isEmpty()){ //  booked
+			for(int j=0; j<bookingInfoList.size(); j++){
+				TableItem item = new TableItem(applicationTable, SWT.NULL);
+				item.setText(0, bookingInfoList.get(j).getVenue().getName() + " at " + bookingInfoList.get(j).getVenue().getLocation() );
+				item.setText(1, bookingInfoList.get(j).getApplicant().getName());
+				item.setText(2, bookingInfoList.get(j).getApplicant().getMatricNo());
+				item.setText(3, bookingInfoList.get(j).getApplicant().getOrganization());
+				item.setText(4, bookingInfoList.get(j).getApplicant().getContact());
+				item.setText(5, bookingInfoList.get(j).getApplicant().getEmail());
+				item.setText(6, bookingInfoList.get(j).getDateTime().toString());
+				if(bookingInfoList.get(j).getStatus()== MACRO.PENDING){
+					item.setText(7,"Pending");
+				}
+				else if (bookingInfoList.get(j).getStatus()== MACRO.APPROVED){
+					item.setText(7,"Approved");
+				}
+				else item.setText(7, "Rejected");
+			}
 		}
 	}
 	
@@ -127,14 +150,7 @@ public class VenueManagement_BookingApplications extends Composite {
 				item.setText(7,"Rejected");
 				
 				/* update the database */
-				//DatabaseReader db = new DatabaseReader();
-				/* ----------------------
-				 * -------------------------
-				 * -----------------
-				 * ---------------------
-				 * ------------
-				 */
-
+				
 				
 			}
 		}
@@ -149,13 +165,7 @@ public class VenueManagement_BookingApplications extends Composite {
 				item.setText(7,"Accepted");
 				
 				/* update the database */
-				//DatabaseReader db = new DatabaseReader();
-				/* ----------------------
-				 * -------------------------
-				 * -----------------
-				 * ---------------------
-				 * ------------
-				 */
+
 			}
 		}
 	}
