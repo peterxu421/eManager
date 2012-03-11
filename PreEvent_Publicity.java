4f0edc0a5fc1e0b51ec20b02172b5044041016dc
Guan Yilun <zoom.aaron@gmail.com>
2012-03-11 15:04:20 +0800

    [Created] Image Publicity

---
+++ PreEvent_Publicity.java
@@ -0,0 +1,19 @@
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class PreEvent_Publicity extends Composite{
+	public PreEvent_Publicity(Composite parent, int style) {
+		super(parent, style);
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 4;
+		this.setLayout(gridLayout);
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
