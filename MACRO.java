ba67563578e5196b150de26a383d4f2543f7b578
Ruifeng Ma <MRFflyer@gmail.com>
2012-03-11 11:41:42 +0800

    [updated] macro

--- MACRO.java
+++ MACRO.java
@@ -4,4 +4,7 @@ public class MACRO {
 	public static int FACILITATOR = 2;
 	public static int PARTICIPANT = 3;
 	public static int APPLICANT = 4;
+	public static int PENDING = 5; 
+	public static int APPROVED = 6;
+	public static int REJECTED = 7;
 }
