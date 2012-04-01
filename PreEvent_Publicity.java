a490d2c821759cc8a44821f3f496002439f68282
Guan Yilun <zoom.aaron@gmail.com>
2012-04-01 13:16:52 +0800

    [Update] PictureImport

--- PreEvent_Publicity.java
+++ PreEvent_Publicity.java
@@ -1,4 +1,6 @@
 
+import java.io.File;
+import org.apache.derby.iapi.services.io.FileUtil;
 import org.eclipse.nebula.widgets.gallery.DefaultGalleryGroupRenderer;
 import org.eclipse.nebula.widgets.gallery.DefaultGalleryItemRenderer;
 import org.eclipse.nebula.widgets.gallery.Gallery;
@@ -59,6 +61,8 @@ public class PreEvent_Publicity extends Composite{
 		group.setText("Images");
 		group.setExpanded(true);
 		
+		fillImages();
+		
 		//right->
 		GridLayout rightLayout = new GridLayout();
 		rightLayout.numColumns = 1;
@@ -68,62 +72,93 @@ public class PreEvent_Publicity extends Composite{
 		GridData importPicData = new GridData(130,50);
 		importPic.setLayoutData(importPicData);
 		importPic.setText("Import");
-		importPic.addSelectionListener(new SelectionAdapter(){
-			public void widgetSelected(SelectionEvent e){
-				Shell open = new Shell(getDisplay());
-				FileDialog browser = new FileDialog(open, SWT.OPEN | SWT.MULTI);
-				browser.setFilterExtensions(new String[]{"*.jpg", "*.png", "*.bmp", "*.*"});
-				browser.open();
-				String[] filenames = browser.getFileNames();
-				String path = browser.getFilterPath();
-				for(int i=0; i<filenames.length; i++){
-					addImage(path +"\\" + filenames[i]);
-				}
-				gallery.redraw();
-			}
-		});
+		importPic.addSelectionListener(new ImportAdapter());
 		
 		Button edit = new Button(right, SWT.PUSH);
 		GridData editData = new GridData(130,50);
 		edit.setText("Edit");
 		edit.setLayoutData(editData);
-		edit.addSelectionListener(new SelectionAdapter(){
-			public void widgetSelected(SelectionEvent e){
-				Shell shell = new Shell(getShell(), SWT.None);
-				shell.setLocation(300,150);
-				
-				if(gallery.getSelection().length!=0){
-					Image image = gallery.getSelection()[0].getImage();
-					PhotoEditorPage page = new PhotoEditorPage(shell, SWT.NONE, image);
-					page.pack();
-					shell.pack();
-					shell.open();
-				}
-			}
-		});
+		edit.addSelectionListener(new EditAdapter());
 		
 		Button delete = new Button(right, SWT.PUSH);
 		GridData deleteData = new GridData(130, 50);
 		delete.setText("Delete");
 		delete.setLayoutData(deleteData);
-		delete.addSelectionListener(new SelectionAdapter(){
-			public void widgetSelected(SelectionEvent e){
-				if(gallery.getSelectionCount()!=0){
-					int length = gallery.getSelection().length;
-					GalleryItem[] images = gallery.getSelection();
-					for(int i=0; i<length; i++){
-						group.remove(images[i]);
-					}
+		delete.addSelectionListener(new DeleteAdapter());
+		
+	}
+	private void addImage(final String path, final String filename){
+		String source = path + File.separator + filename;
+		String target = "Picture" + File.separator + filename; 
+		File sourceFile = new File(source);
+		File targetFile = new File(target);
+		FileUtil.copyFile(sourceFile, targetFile);
+		Image image = new Image(getDisplay(), new ImageData("Picture" + File.separator + filename));
+		if(image!=null){
+			GalleryItem item = new GalleryItem(group, SWT.None);
+			item.setImage(image);
+			item.setText(filename);
+		}
+	}
+	private void fillImages(){
+		File imageDirectory = new File("Pictures");
+		if(!imageDirectory.exists()) imageDirectory.mkdir();
+		
+		String[] images = imageDirectory.list();
+		for(int i=0; i<images.length; i++){
+			Image image = new Image(getDisplay(), new ImageData("Pictures" + File.separator + images[i]));
+			if(image!=null){
+				GalleryItem item = new GalleryItem(group, SWT.None);
+				item.setImage(image);
+				item.setText(images[i]);
+			}
+		}
+	}
+	
+	private class ImportAdapter extends SelectionAdapter{
+		public void widgetSelected(SelectionEvent e){			
+			Shell open = new Shell(getDisplay());
+		
+			FileDialog browser = new FileDialog(open, SWT.OPEN | SWT.MULTI);
+			browser.setFilterExtensions(new String[]{"*.jpg", "*.png", "*.bmp", "*.*"});
+			browser.open();
+			
+			String[] filenames = browser.getFileNames();
+			String path = browser.getFilterPath();
+			String targetPath = "Pictures";
+			
+			for(int i=0; i<filenames.length; i++){
+				String target = targetPath + File.separator + filenames[i];
+				addImage(path, filenames[i]);
+
+			}
+			gallery.redraw();
+		}
+	}
+	private class EditAdapter extends SelectionAdapter{
+		public void widgetSelected(SelectionEvent e){
+			Shell shell = new Shell(getShell(), SWT.None);
+			shell.setLocation(300,150);
+			if(gallery.getSelection().length!=0){
+				Image image = gallery.getSelection()[0].getImage();
+				PhotoEditorPage page = new PhotoEditorPage(shell, SWT.NONE, image);
+				page.pack();
+				shell.pack();
+				shell.open();
+			}
+		}
+	}
+	private class DeleteAdapter extends SelectionAdapter{
+		public void widgetSelected(SelectionEvent e){
+			if(gallery.getSelectionCount()!=0){
+				int length = gallery.getSelection().length;
+				GalleryItem[] images = gallery.getSelection();
+				for(int i=0; i<length; i++){
+					group.remove(images[i]);
+					File file = new File("Pictures" + File.separator + images[i].getText());
+					file.delete();
 				}
 			}
-		});
-	}
-	public void addImage(final String path){
-		Image image = new Image(getDisplay(), new ImageData(path));
-		GalleryItem item = new GalleryItem(group, SWT.None);
-		item.setText(path);
-		if(image!=null){
-			item.setImage(image);
 		}
 	}
 	
